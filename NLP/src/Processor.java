62ee538c6b929c71fec72060b2a78662b5b78572
Chang Su <changsu@stanford.edu>
2012-11-16 02:06:48 -0800

    build file reading and sentence constructor

---
+++ NLP/src/Processor.java
@@ -0,0 +1,148 @@
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringReader;
+
+import edu.stanford.nlp.objectbank.TokenizerFactory;
+import edu.stanford.nlp.process.CoreLabelTokenFactory;
+import edu.stanford.nlp.process.DocumentPreprocessor;
+import edu.stanford.nlp.process.PTBTokenizer;
+import edu.stanford.nlp.ling.CoreLabel;  
+import edu.stanford.nlp.ling.HasWord;  
+import edu.stanford.nlp.ling.Sentence;  
+import edu.stanford.nlp.trees.*;
+import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * The processor is the top level controller of the whole project and
+ * it has several functionalities as below: 
+ * 1. Read in files/documents
+ * 2. Output labeled or unlabeled output as input to MATLAB 
+ * 3. Output relations to database for query
+ */
+
+public class Processor {
+	 
+	private String inputFileName;
+	private String outputFileName;
+	
+	/* construct list of sentence object after parsing the JSON file */
+	private ArrayList<tSentence> sentences;
+	
+	/* construct list of relation object as samples */
+	private ArrayList<Relation> relations;
+	
+	public Processor(String inputFileName, String outputFileName){
+		this.inputFileName = inputFileName;
+		this.outputFileName = outputFileName;
+		sentences = new ArrayList<tSentence>();
+		relations = new ArrayList<Relation>();
+	}
+	
+	/**
+	 * Read and parse input file of JSON string and populate 
+	 * list of sentences, from where relations will be extracted
+	 */
+	public void readFile() {
+		File inf = new File("files/" + this.inputFileName);
+		if(!inf.exists()) { 
+			System.err.println("ERROR: File: "+inf.getAbsolutePath()+" does not exist");
+			return;
+		}
+		if(!inf.canRead()) { 
+			System.err.println("ERROR: File: "+inf.getAbsolutePath()+" cannot be read");
+			return;
+		}
+		
+		System.out.println("INFO: finish reading file" + inf.getAbsolutePath());
+		parseJSONFile(inf);
+		printSentences();
+	}
+	
+	/**
+	 * Parse the input JSON file, construct sentence object from each JSON
+	 * object for further process and populate the sentence list
+	 * @param inf
+	 */
+	public void parseJSONFile(File inf) {
+		/* convert content of the file into string */
+		try {
+			BufferedReader reader;
+			reader = new BufferedReader(new FileReader(inf));
+
+			String line = null;
+			StringBuilder  stringBuilder = new StringBuilder();
+
+			while( ( line = reader.readLine() ) != null ) {
+				stringBuilder.append( line );
+			}
+			
+			// Parse the JSON string to JSON array
+			JSONArray records = new JSONArray(stringBuilder.toString());
+			
+			// construct one sentence object for each JSON object in JSON array
+			for (int i = 0; i < records.length(); ++i) {
+				JSONObject record = records.getJSONObject(i);
+				tSentence sentence = new tSentence(record.getString("url"), 
+												   record.getString("body"));
+				sentences.add(sentence);
+			}			
+			// close input stream
+			reader.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		} catch (JSONException e2) {
+			e2.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+//		LexicalizedParser lp = LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
+		if (args.length != 2) {
+			System.err.println("usage: Processor inputfilename outputfilename");
+			return;
+		} else {
+			Processor processor = new Processor(args[0], args[1]);
+			processor.readFile();
+		}
+	}
+
+//	public static void parseF(LexicalizedParser lp, String filename) {
+//		// This option shows loading and sentence-segment and tokenizing
+//		// a file using DocumentPreprocessor
+//		TreebankLanguagePack tlp = new PennTreebankLanguagePack();
+//		GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
+//		// You could also create a tokenizer here (as below) and pass it
+//		// to DocumentPreprocessor
+//		for (List<HasWord> sentence : new DocumentPreprocessor(filename)) {
+//			Tree parse = lp.apply(sentence);
+//			parse.pennPrint();
+//
+//			GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+//			Collection tdl = gs.typedDependenciesCCprocessed(true);
+//			System.out.println(tdl);
+//		}
+//	}
+//
+	
+	/**
+	 * Set of utility print functions for debugging
+	 */
+	private void printSentences() {
+		for (tSentence sentence : sentences) {
+			System.out.println(sentence.toString());
+		}
+	}
+}
--- NLP/src/tSentence.java
+++ NLP/src/tSentence.java
@@ -1,28 +1,41 @@
-import java.util.List;
 
-import edu.stanford.nlp.ling.HasWord;
+import java.awt.List;
+import java.util.ArrayList;
 
+/**
+ * This Class define sentence object that stores
+ * sentence source and also list of relations
+ * extracted from that sentence
+ */
 
 public class tSentence {
-	private List<HasWord> sentence;
-	private String sentenceSource; 
+
+	private String url;
+	private String body;
 	private ArrayList<Relation> relations;
 	
-	public tSentence(List<HasWord> sentence) {
-		this.sentence = sentence;
-}
+	public tSentence(String url, String body) {
+		this.url = url;
+		this.body = body;
+		relations = new ArrayList<Relation>();
+	}
 
-	public void setRelations(ArrayList<Relation> relations) {
-		this.relations = relations;
-}
+	public String getURL() {
+		return url;
+	}
+	
+	public ArrayList<Relation> getRelations() {
+		return relations;
+	}
 
-public List<HasWord> getSentence() {
-	return sentence;
-}
-
-public ArrayList<Relation> getRelations() {
-	return relations;
-}
-
-
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("url: ");
+		sb.append(url);
+		sb.append("\n");
+		sb.append("body: ");
+		sb.append(body);
+		sb.append("\n");
+		return sb.toString();
+	}
 }
