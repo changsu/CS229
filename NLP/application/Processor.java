74884c6c2cc5342b09c8c0fa95dbaf5650038eff
Chang Su <changsu@stanford.edu>
2012-11-28 23:28:01 -0800

    add full file

--- NLP/application/Processor.java
+++ NLP/application/Processor.java
@@ -177,10 +177,10 @@ public class Processor {
 	private void extractRelations() {
 		LexicalizedParser lp = 
 				LexicalizedParser.loadModel("edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz");
-//		relations.addAll(articles.get(8).extractRelations(lp));
-		for (Article article : articles) {
-			relations.addAll(article.extractRelations(lp));
-		}
+		relations.addAll(articles.get(9).extractRelations(lp));
+//		for (Article article : articles) {
+//			relations.addAll(article.extractRelations(lp));
+//		}
 	}
 	
 	/**
